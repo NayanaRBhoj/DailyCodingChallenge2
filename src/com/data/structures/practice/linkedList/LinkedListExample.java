0d6209ff4fea212fa1130727f0a734611b3ee3ea
NayanaBhoj-Ahold <nayana.bhoj@gmail.com>
2024-01-09 01:15:05 +0530

    More problems list added

--- src/com/data/structures/practice/linkedList/LinkedListExample.java
+++ src/com/data/structures/practice/linkedList/LinkedListExample.java
@@ -2,28 +2,28 @@ package com.data.structures.practice.linkedList;
 
 public class LinkedListExample {
 
-	public static class LinkedListNode {
+	public static class Node {
 		int val;
-		LinkedListNode next = null;
+		Node next = null;
 
-		public LinkedListNode(int val) {
+		public Node(int val) {
 			this.val = val;
 		}
 
-		public LinkedListNode() {
+		public Node() {
 
 		}
 	}
 
-	public static LinkedListNode head = null;
-	public static LinkedListNode tail = null;
+	public static Node head = null;
+	public static Node tail = null;
 
 	/**
 	 * Add Node to first of LL
 	 */
-	public static void addNodeFirst(int val) {
+	public void addNodeFirst(int val) {
 		// Step1: Create new node.
-		LinkedListNode newNode = new LinkedListNode(val);
+		Node newNode = new Node(val);
 
 		if (head == null) {
 			head = tail = newNode;
@@ -40,24 +40,20 @@ public class LinkedListExample {
 	/**
 	 * Add Node to last of LL
 	 */
-	public static void addNodeLast(int val) {
+	public void addNodeLast(int val) {
 		// Step1: Create new node.
-		LinkedListNode newNode = new LinkedListNode(val);
+		Node newNode = new Node(val);
 
 		if (head == null) {
 			head = tail = newNode;
 			return;
 		}
-		LinkedListNode current = head;
-		while (current != null) {
-			current = current.next;
-		}
-		// Step2: newNode.next = head
-		current.next = newNode;
-		tail = newNode;
+		// Step2: tail.next = newNode
+		tail.next = newNode;
 		
-		// Step3: head = newNode
-		//head = newNode;
+		
+		// Step3: tail = newNode
+		tail = newNode;
 	}
 	
 	/**
@@ -65,8 +61,16 @@ public class LinkedListExample {
 	 */
 
 	/**
-	 * Add at K'th index in LL
+	 * Add Node at K'th index from start/first in LL
 	 */
+	
+	/**
+	 * Add Node at K'th index from last in LL
+	 */
+	
+	/**
+	 * Find size of LL
+	 * */
 
 	/**
 	 * Remove first Node of LL
@@ -87,16 +91,59 @@ public class LinkedListExample {
 	/**
 	 * Remove Node at K'th index from last in LL
 	 */
+	
+	/**
+	 * Check if there is Node with value K in LL
+	 * */
+	
+	/**
+	 *  Remove all Nodes with even values in LL
+	 * */
+	
+	
+	/** 
+	 * Reverse LL
+	 * */
+	
+	/**
+	 * Leetcode : 203 (Easy) Remove Linked List Elements
+	 * Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+	 * Example 1 Input: head = [1,2,6,3,4,5,6], val = 6 Output: [1,2,3,4,5]
+	 * Exampple 2 Input: head = [], val = 1 Output: []
+	 * Exampple 3 Input: head = [7,7,7,7], val = 7 Output: []
+	 * */
 
+	/**
+	 * Leetcode : 876 (Easy) Middle of the Linked List
+	 * Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.
+	 * Example 1 Input: head = [1,2,3,4,5] Output: [3,4,5] Explanation: The middle node of the list is node 3.
+	 * Example 2 Input: head = [1,2,3,4,5,6] Output: [4,5,6] Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+	 * */
+	
+	/**
+	 * Leetcode : 206 (Easy) Reverse Linked List
+	 * Given the head of a singly linked list, reverse the list, and return the reversed list.
+	 * Example 1 Input: head = [1,2,3,4,5] Output: [5,4,3,2,1]
+	 * Example 2 Input: head = [1,2] Output: [2,1]
+	 * Example 3 Input: head = [] Output: []
+	 * */
+	
+	/**
+	 * Leetcode : 2816 (Medium) Double a Number Represented as a Linked List
+	 * You are given the head of a non-empty linked list representing a non-negative integer without leading zeroes. Return the head of the linked list after doubling it.
+	 * Example 1 Input: head = [1,8,9] Output: [3,7,8] Explanation: The figure above corresponds to the given linked list which represents the number 189. Hence, the returned linked list represents the number 189 * 2 = 378.
+	 * Example 2 Input: head = [9,9,9] Output: [1,9,9,8] Explanation: The figure above corresponds to the given linked list which represents the number 999. Hence, the returned linked list reprersents the number 999 * 2 = 1998. 
+	 * */
+	
 	/**
 	 * Print LL with given head of LL.
 	 */
-	public static void printLL(LinkedListNode head) {
+	public static void printLL() {
 		if (head == null) {
-			System.out.println("LinkedList is null");
+			System.out.println("LinkedList is empty");
 			return;
 		}
-		LinkedListNode current = head;
+		Node current = head;
 		while (current != null) {
 			System.out.print(current.val + "->");
 			current = current.next;
@@ -107,8 +154,12 @@ public class LinkedListExample {
 	public static void main(String[] args) {
 
 		LinkedListExample ll = new LinkedListExample();
-		ll.addNodeFirst(1);
+		printLL();
 		ll.addNodeFirst(2);
-		printLL(head);
+		ll.addNodeFirst(1);
+		ll.addNodeLast(3);
+		ll.addNodeLast(4);
+		ll.addNodeLast(5);
+		printLL();
 	}
 }
